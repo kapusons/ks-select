a5ef7dcdbf1e71af050858236f4e76a732fbf50d
dan721 <d.dibella@kapusons.it>
2023-02-19 16:04:18 +0100

    cleanup

--- src/KsSelect/KsNotMappedAttribute.cs
+++ src/KsSelect/KsNotMappedAttribute.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace Kapusons.Components
 {
 	/// <summary>
 	/// Denotes that an element should be excluded from mapping/serialization.
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Class| AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false)]
 	public class KsNotMappedAttribute : Attribute { }
 }
--- src/KsSelect/Util/PredicateBuilder.cs
+++ src/KsSelect/Util/PredicateBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
